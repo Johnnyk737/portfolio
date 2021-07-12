f33084594227ec85abade0d248b4fd9d9e2e8442
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2021-07-12 21:04:24 +0000

    Deploy Johnnyk737/portfolio to github.com/Johnnyk737/portfolio.git:gh-pages

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import { Sidebar } from './components/Sidebar';
+import { Main } from './components/Main';
+import './App.css';
+import './styles/main.css';
+
+function App() {
+  return (
+    <div className="App">
+      <Sidebar />
+      <Main />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/utils/ImageHelper.js
@@ -0,0 +1,41 @@
+import cssIcon from '../assets/img/css3-original.svg';
+import githubIcon from '../assets/img/github-original.svg';
+import githubOriginal from '../assets/img/github-original-wordmark.svg';
+import gulpIcon from '../assets/img/gulp-plain.svg';
+import gruntIcon from '../assets/img/grunt-original.svg';
+import htmlIcon from '../assets/img/html5-original.svg';
+import intellijIcon from '../assets/img/intellij-original.svg';
+import javaIcon from '../assets/img/java-original.svg';
+import jqueryIcon from '../assets/img/jquery-original.svg';
+import linkedInIcon from '../assets/img/In-Black-94px-TM.png';
+import mysqlIcon from '../assets/img/mysql-original.svg';
+import nodeJsIcon from '../assets/img/nodejs-original.svg';
+import npmIcon from '../assets/img/npm-original-wordmark.svg';
+import pycharmIcon from '../assets/img/pycharm-original.svg';
+import pythonIcon from '../assets/img/python-original.svg';
+import railsIcon from '../assets/img/ruby-on-rails-icon.svg'
+import reactIcon from '../assets/img/react-original.svg';
+import rubyIcon from '../assets/img/ruby-original.svg';
+
+const images = {
+  cssIcon: cssIcon,
+  githubIcon: githubIcon,
+  githubOriginal: githubOriginal,
+  gulpIcon: gulpIcon,
+  gruntIcon: gruntIcon,
+  htmlIcon: htmlIcon,
+  intellijIcon: intellijIcon,
+  javaIcon: javaIcon,
+  jqueryIcon: jqueryIcon,
+  linkedInIcon: linkedInIcon,
+  mysqlIcon: mysqlIcon,
+  nodeJsIcon: nodeJsIcon,
+  npmIcon: npmIcon,
+  pycharmIcon: pycharmIcon,
+  pythonIcon: pythonIcon,
+  railsIcon: railsIcon,
+  reactIcon: reactIcon,
+  rubyIcon: rubyIcon
+}
+
+export default images;
